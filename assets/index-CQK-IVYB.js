03a532a458763ecb899ecd9cfe56d21873a5d2f6
abdulrahman-shoaib2 <abdulrahman.shoaib012@gmail.com>
2026-02-22 17:23:46 +0200

    Delete assets directory

--- assets/index-CQK-IVYB.js
+++
@@ -1,1 +0,0 @@
-import{G as a}from"./features-animation-Co2Pu3q-.js";import"./index-BwbPjsOj.js";var i=a;export{i as default};
--- assets/src-UW24ZMRV-B6PlqnP6.js
+++
@@ -1,1 +0,0 @@
-import{G as a}from"./features-animation-Co2Pu3q-.js";import"./index-BwbPjsOj.js";var t=a;export{t as default};
